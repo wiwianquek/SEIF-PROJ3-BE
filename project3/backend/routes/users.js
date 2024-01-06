fcf3f42d0567466979cfb4ba58a89b551ad83e68
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2024-01-06 09:46:42 +0800

    Backend Prep
    - Added mongo.js under client
    - Added respective folders like controllers/daos/models
    - Added .env
    - Added require for dotenv and mongo in server.js

--- project3/backend/routes/users.js
+++ project3/backend/routes/users.js
@@ -1,25 +1,9 @@
 var express = require('express');
-var userController = require('../controllers/users')
-
 var router = express.Router();
 
-
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-router.get("/", userController.getUsers);
-router.post("/create", userController.createUser); // add this route
-
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
-
-//to test on postman
-// {
-//     "first_name": "John",
-//     "last_name": "Doe",
-//     "username": "john.doe",
-//     "email": "johndoe@example.com",
-//     "password": "aSecurePassword123"
-// }
