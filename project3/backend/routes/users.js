c06cdb887b76c8e675bcd4b21c9ec852782e4071
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2024-01-13 16:35:39 +0800

    refactor route for create user
    - instead of /create, changed to /create-user route for user schema

--- project3/backend/routes/users.js
+++ project3/backend/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var userController = require('../controllers/users')
+
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get("/", userController.getUsers);
+router.post("/create-user", userController.createUser); // add this route
+
 
 module.exports = router;
+
+//to test on postman
+// {
+//     "first_name": "John",
+//     "last_name": "Doe",
+//     "username": "john.doe",
+//     "email": "johndoe@example.com",
+//     "password": "aSecurePassword123"
+// }
