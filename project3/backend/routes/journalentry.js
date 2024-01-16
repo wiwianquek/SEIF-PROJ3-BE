a9e99fab02cec23d29d959df4653dc92db6ece74
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2024-01-17 00:15:42 +0800

    Update journalentry.js

--- project3/backend/routes/journalentry.js
+++ project3/backend/routes/journalentry.js
@@ -10,3 +10,13 @@ router.post("/create-journal-entry", journalController.createJournalEntry);
 
 module.exports = router;
 
+//to test on postman
+// {
+//     "user_id": "65a677619646798d1980e068",
+//     "card_id": "65a6aa0d2302804f074118af",
+//     "entry_title": "testing FIRSTTTT for foreign key",
+//     "entry_description": "test test test",
+//     "entry_text": "testingggg",
+//     "date": "2024-01-16"
+//   }
+  
