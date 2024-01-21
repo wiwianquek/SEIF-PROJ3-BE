9e326e0f0d6584cac53575c70ecaef58cecd6d5c
alexpanmk <alexpanmk@gmail.com>
2024-01-21 09:43:34 +0800

    fix: add login functions

--- project3/backend/models/users.js
+++ project3/backend/models/users.js
@@ -1,12 +1,55 @@
+const { urlencoded } = require("express");
 const daoUser = require("../daos/users")
+const utilSecurity = require("../util/security")
 
 module.exports = {
     getUsers,
+    getLoginDetails,
+    loginUser,
     createUser
   };
 
 function getUsers(queryFields) {
     return daoUser.find(queryFields);
+}
+
+async function getLoginDetails(queryFields) {
+  const loginFields = {
+    name : 1,
+    salt: 1,
+    iterations: 1
+  } 
+  if (!queryFields.hasOwnProperty("email")){
+    return {success: false, error: "missing email"};
+  }
+  // url decode email '@' -> %40 
+  const userEmail = decodeURIComponent(queryFields.email);
+  const loginFieldsRes = await daoUser.findOne({"email": userEmail}, loginFields);
+  return {success: true, data: loginFieldsRes};
+}
+
+async function loginUser(body) {
+  if (!body.hasOwnProperty("email")) {
+    return {success: false, error: "missing email"};
+  }
+  if (!body.hasOwnProperty("password")) {
+    return {success: false, error: "missing password"};
+  }
+
+  const user = await daoUser.findOne({"email": body.email, "password": body.password});
+  if (user == null || Object.keys(user).length == 0) {
+    return {success: false, error: "Invalid email/password"};
+  }
+  
+  const jwtPayload = {
+    user: user.name,
+    email: user.email,
+    is_admin: user.is_admin
+  };
+  const token = utilSecurity.createJWT(jwtPayload);
+  const expiry = utilSecurity.getExpiry(token);
+  daoUser.updateOne({"email": body.email}, {token: token, expire_at: expiry})
+  return {success: true, data: token}
 }
 
 function createUser(user) {
