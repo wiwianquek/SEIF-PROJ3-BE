daecdd328f2847d9b47bc8bba2dd15363a38fc8e
alexpanmk <alexpanmk@gmail.com>
2024-01-23 08:37:19 +0800

    add: getQuotes service

---
+++ project3/backend/services/quoteservice.js
@@ -0,0 +1,21 @@
+//service to fetch quotes for the month
+
+const axios = require('axios');
+const config = require('../config/config');
+const common = require('../util/common');
+
+module.exports = {
+    getQuotes
+}
+
+//function will return required array of quotes for the month
+async function getQuotes(month, year, frequency) {
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const quotesToFetch = Math.ceil(daysInMonth / frequency);
+    const quotes = [];
+    for (let i = 0; i < quotesToFetch; i++) {
+        const quote = await getQuote();
+        quotes.push(quote);
+    }
+    return quotes;
+}
