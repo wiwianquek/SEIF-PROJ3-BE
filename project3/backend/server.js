940aa453fe5aee81a81efdff67dbb9226661aa19
BenjaminMalachi <141989628+BenjaminMalachi@users.noreply.github.com>
2024-01-27 00:48:31 +0800

    changed back to local host deployment

--- project3/backend/server.js
+++ project3/backend/server.js
@@ -6,27 +6,19 @@ var logger = require('morgan');
 var cors = require('cors');
 var securityMiddleware = require('./middlewares/security');
 
-// Routes
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var daycardRouter = require('./routes/daycard');
 var journalentryRouter = require('./routes/journalentry');
 var carddisplayRouter = require('./routes/carddisplay');
 
-// Load environment variables
 require("dotenv").config();
-
-// Connect to MongoDB or any database you use
 require("./client/mongo");
 
 var app = express();
 
-const corsOptions = {
-  origin: 'https://elysio.vercel.app',
-  optionsSuccessStatus: 200 // For legacy browser support
-};
-
-// View engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -35,40 +27,109 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// CORS Middleware setup
-app.use(cors({ origin: 'https://elysio.vercel.app' })); // Enable CORS for the frontend app
-
-// Enable preflight requests for all routes
-app.options('*', cors({ origin: 'https://elysio.vercel.app' })); // This handles preflight requests
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
-// Security middleware, if it's used for checking JWT it should be after CORS setup
+app.use(cors());
 app.use(securityMiddleware.checkJWT);
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/daycard', daycardRouter);
 app.use('/journal', journalentryRouter);
 app.use('/carddisplay', carddisplayRouter);
 
-// Catch 404 and forward to error handler
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.title = 'Error'; // Define the title for the error page
 
-  // Render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+
 module.exports = app;
+
+
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// // var cors = require('cors');
+// var securityMiddleware = require('./middlewares/security');
+
+// // Routes
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var daycardRouter = require('./routes/daycard');
+// var journalentryRouter = require('./routes/journalentry');
+// var carddisplayRouter = require('./routes/carddisplay');
+
+// // Load environment variables
+// require("dotenv").config();
+
+// // Connect to MongoDB or any database you use
+// require("./client/mongo");
+
+// var app = express();
+
+// // const corsOptions = {
+// //   origin: 'https://elysio.vercel.app',
+// //   optionsSuccessStatus: 200 // For legacy browser support
+// // };
+
+// // View engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // // CORS Middleware setup
+// // app.use(cors({ origin: 'https://elysio.vercel.app' })); // Enable CORS for the frontend app
+
+// // // Enable preflight requests for all routes
+// // app.options('*', cors({ origin: 'https://elysio.vercel.app' })); // This handles preflight requests
+
+// // app.use(cors(corsOptions));
+// // app.options('*', cors(corsOptions));
+
+// // Security middleware, if it's used for checking JWT it should be after CORS setup
+// app.use(securityMiddleware.checkJWT);
+
+// // Routes
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/daycard', daycardRouter);
+// app.use('/journal', journalentryRouter);
+// app.use('/carddisplay', carddisplayRouter);
+
+// // Catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // Error handler
+// app.use(function(err, req, res, next) {
+//   // Set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // Render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
